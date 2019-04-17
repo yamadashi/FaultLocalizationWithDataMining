f4f943f61cb037b5278f556369a8560272aee093
yamadashi <usaming27@gmail.com>
2019-04-17 16:10:09 +0900

    collect test data

---
+++ testdata/testdata.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd `dirname $0`
+outputfile="testdata"
+
+function random() {
+    echo $(( $(od -vAn --width=4 -tu4 -N4 </dev/urandom) % 10 ))
+}
+
+function output() {
+    local input1=`random`
+    local input2=`random`
+    local input3=`random`
+
+    echo -n "$input1 $input2 $input3 " >> $outputfile
+
+    #main.exeで書き込みしている
+    ./src/main.exe $outputfile $input1 $input2 $input3
+}
+
+for i in `seq 400`
+do
+   output
+done
