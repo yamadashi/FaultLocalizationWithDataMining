789c6dfbdabd49b8392e2bc047f94c1c86fe5b02
ymds <usaming27@gmail.com>
2019-12-27 15:47:15 +0900

    update

--- CalcConcepts/src/ConceptWrapper.java
+++ CalcConcepts/src/ConceptWrapper.java
@@ -11,14 +11,16 @@ class ConceptWrapper extends Concept {
 
     @Override
     public boolean checkPPC(int current, Concept prev, int[] upto, int[] negObjs) {
-        // 成功集合を保存
+        // 成功集合を保存する場合はfalseを返す
         int[] diff = SetOperation.xor(prev.getExtent(), extent);
         int[] diffofNegObjs = SetOperation.intersection(diff, negObjs);
-        if (SetOperation.size(diffofNegObjs) == 0) { // ppcExtMaskの更新と探索の打ち切り
+        if (SetOperation.size(diffofNegObjs) == 0) {
+            // ppcExtMaskの更新と探索の打ち切り
             prev.ppcExtMask[current / INTSIZE] |= (BIT << (INTSIZE - 1 - current % INTSIZE));
             ppcExtMask = prev.ppcExtMask.clone();
             return false;
         }
+
         return super.checkPPC(current, prev, upto, null);
     }
 }
