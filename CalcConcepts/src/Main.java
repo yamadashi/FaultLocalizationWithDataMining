5dbb46255e35ad5904f6c7277e293aaeb73f07e1
ymds <usaming27@gmail.com>
2019-12-30 22:54:25 +0900

    update

--- CalcConcepts/src/Main.java
+++ CalcConcepts/src/Main.java
@@ -2,10 +2,7 @@ public class Main {
 	public static void main(String[] args) {
 		String file = args[0];
 		String filter = args[1];
-		float minsup = 0f;
-		if (filter.equals("support") || filter.equals("all")) {
-			minsup = Float.parseFloat(args[2]);
-		}
+		float minsup = 0.01f * Integer.parseInt(args[2]);
 
 		System.out.println("#target: " + file + ", filter:" + filter + ", minsup:" + minsup);
 		new CloseByOne(file, filter, minsup).run();
---
+++ CalcConcepts/tritype_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+filters=("support" "all")
+
+ls data/tritype/ -1 | while read each_case; do
+    outputdir=output/tritype/${each_case}
+    outputfile=${outputdir}/output.txt
+    # make directory
+    mkdir -p ${outputdir} 2> /dev/null
+    # reset output file
+    rm ${outputfile} 2> /dev/null
+    touch ${outputfile}
+    chmod 777 ${outputfile}
+
+    for filter in ${filters[@]}; do
+        echo -e "${filter}\n" >> ${outputfile}
+        make exec \
+        INPUT=data/tritype/${each_case}/data/data.csv \
+        FILTER=${filter} \
+        MINSUP=1 \
+        >> ${outputfile}
+        echo -e "\n\n" >> ${outputfile}
+    done
+done
