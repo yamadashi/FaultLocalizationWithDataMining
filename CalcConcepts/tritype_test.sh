f9f06383723cd1d3a5d7c4be6e1aa96a72b2c535
yamadashi <usaming27@gmail.com>
2020-01-07 18:28:42 +0900

    update

--- CalcConcepts/tritype_test.sh
+++ CalcConcepts/tritype_test.sh
@@ -16,7 +16,7 @@ ls data/tritype/ -1 | while read each_case; do
     for filter in ${filters[@]}; do
         echo -e "${filter}\n" >> ${outputfile}
         make exec \
-        INPUT=data/tritype/${each_case}/data/data.csv \
+        INPUT=data/tritype/${each_case}/data/adjust.txt \
         FILTER=${filter} \
         MINSUP=1 \
         >> ${outputfile}
